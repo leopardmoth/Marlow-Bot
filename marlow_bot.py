feab2e02493d8d9464353172a93b8b2d4de72308
Kim <lekim451@gmail.com>
2019-03-19 02:28:17 -0500

    PEP-8 complaint, new commands

--- marlow_bot.py
+++ marlow_bot.py
@@ -1,23 +1,9 @@
 # import necessary modules
 import random
-import json
 import discord
 import asyncio
-import time
-import pylast
-import colorgram
-import os
-import os.path
-import re
-import io
-import typing
 import config
-import typing
-import imghdr
-from PIL import Image,ImageDraw
 from discord.ext import commands
-from urllib.request import urlopen, Request
-from aiohttp import ClientSession
 
 # creates a bot instance with "$" as the command prefix
 bot = commands.Bot("$")
@@ -25,13 +11,13 @@ client = discord.Client()
 
 TOKEN = config.TOKEN
 
-#stage changes & commit
-#git fetch origin master
-#git push origin master
-header = {"User-Agent" : "Magic Browser"}
+# stage changes & commit
+# git fetch origin master
+# git push origin master
+header = {"User-Agent": "Magic Browser"}
 heart = '<a:loading_heart:542883297600995349>'
-staff = {'Admin':553314578528993311,
-        'Mod':553356112636936203}
+staff = {'Admin': 553314578528993311,
+         'Mod': 553356112636936203}
 muted = 553358001550131208
 
 # This is how you define a discord.py event
@@ -41,29 +27,35 @@ async def on_ready():  # the event `on_ready` is triggered when the bot is ready
     print("Logged in as: {}".format(bot.user.name))
     bot.load_extension("cogs.Utility")
     bot.load_extension("cogs.Fun")
-    await bot.change_presence(activity=discord.Activity(name="Despacito",type=discord.ActivityType.listening))
+    await bot.change_presence(activity=discord.Activity(name="Despacito", type=discord.ActivityType.listening))
+
 
 @bot.event
-async def on_command_error(ctx,e):
+async def on_command_error(ctx, e):
     if isinstance(e, commands.CommandOnCooldown):
         message = await ctx.send(f'Stop spamming! Try again in {round(e.retry_after)+1} second(s).')
-    if isinstance(e, commands.CheckFailure):
+    elif isinstance(e, commands.CheckFailure):
         message = await ctx.send(f"Fuck off! You're not my master!")
-    if isinstance(e, commands.CommandNotFound):
+    elif isinstance(e, commands.CommandNotFound):
         message = await ctx.send(f"Command does not exist.")
-    if isinstance(e, commands.MissingPermissions):
+    elif isinstance(e, commands.MissingPermissions):
         message = await ctx.send(f"You are missing these ({list.missing_perms}) to run this command.")
-    if isinstance(e, commands.BotMissingPermissions):
+    elif isinstance(e, commands.BotMissingPermissions):
         message = await ctx.send(f"I need these permissions ({list.missing_perms}) to run this command.")
-    if isinstance(e, commands.CommandError):
+    elif isinstance(e, commands.CommandError):
+        message = await ctx.send(f"**Error:** {e}")
+    elif isinstance(e, discord.HTTPException):
         message = await ctx.send(f"**Error:** {e}")
     else:
         print(e)
         return
     await asyncio.sleep(5)
     await message.delete()
-    try: await ctx.message.delete()
-    except discord.HTTPException: pass
+    try:
+        await ctx.message.delete()
+    except discord.HTTPException:
+        pass
+
 
 @bot.event
 async def on_message(message):
@@ -76,16 +68,18 @@ async def on_message(message):
             await message.channel.send('whats this?')
     await bot.process_commands(message)
 
+
 @bot.event
 async def on_member_join(member):
     channel = bot.get_channel(555216652589858816)
-    message ='Hello {}! Please enjoy your stay.'.format(member.mention)
+    message = 'Hello {}! Please enjoy your stay.'.format(member.mention)
     await channel.send(message)
+
 
 @bot.event
 async def on_member_remove(member):
     channel = bot.get_channel(555216652589858816)
-    message ='Goodbye {}! We will miss you!'.format(member.mention)
+    message = 'Goodbye {}! We will miss you!'.format(member.mention)
     await channel.send(message)
 
 initial_extensions = ["cogs.Utility,cogs.Fun"]
@@ -96,7 +90,8 @@ if __name__ == '__cogs__':
         try:
             bot.load_extension(extension)
         except Exception as error:
-            print('{} cannot be loaded. [{}]'.format(extension,error))
+            print('{} cannot be loaded. [{}]'.format(extension, error))
+
 
 @bot.command()
 @commands.is_owner()
@@ -106,8 +101,9 @@ async def unload(ctx, extension_name: str):
         bot.unload_extension(extension_dir)
         await ctx.send("{} unloaded.".format(extension_name))
     except Exception as error:
-        await ctx.send('{} cannot be unloaded. [{}]'.format(extension_name,error))
-        print('{} cannot be unloaded. [{}]'.format(extension_name,error))
+        await ctx.send('{} cannot be unloaded. [{}]'.format(extension_name, error))
+        print('{} cannot be unloaded. [{}]'.format(extension_name, error))
+
 
 @bot.command()
 @commands.is_owner()
@@ -117,8 +113,21 @@ async def load(ctx, extension_name: str):
         bot.load_extension(extension_dir)
         await ctx.send("{} loaded.".format(extension_name))
     except Exception as error:
-        await ctx.send('{} cannot be loaded. [{}]'.format(extension_name,error))
-        print('{} cannot be loaded. [{}]'.format(extension_name,error))
+        await ctx.send('{} cannot be loaded. [{}]'.format(extension_name, error))
+        print('{} cannot be loaded. [{}]'.format(extension_name, error))
+
+
+@bot.command()
+@commands.is_owner()
+async def update(ctx, extension_name: str):
+    try:
+        extension_dir = f"{cogs_dir}.{extension_name}"
+        bot.unload_extension(extension_dir)
+        bot.load_extension(extension_dir)
+        await ctx.send("{} unloaded and loaded".format(extension_name))
+    except Exception as error:
+        await ctx.send('{} cannot be loaded. [{}]'.format(extension_name, error))
+        print('{} cannot be loaded. [{}]'.format(extension_name, error))
 
 # starts the bot with the corresponding token
 bot.run(TOKEN)
