b2c2bdf33cd998e7f8ff3a6c2b522a573e9cd1ad
Kim <lekim451@gmail.com>
2020-01-29 00:57:47 -0600

    Update to Discord.py 1.3.1, Updated commands`
    She's back.

--- cogs/Utility.py
+++ cogs/Utility.py
@@ -6,15 +6,14 @@ import typing
 from discord.ext import commands
 from aiohttp import ClientSession
 
-header = {"User-Agent": "Magic Browser"}
+header = {"User-Agent" : "Magic Browser"}
 heart = '<a:loading_heart:542883297600995349>'
-staff = {'Admin': 553314578528993311,
-         'Mod': 553356112636936203}
+staff = {'Admin':553314578528993311,
+        'Mod':553356112636936203}
 muted = 553358001550131208
 
-
-class Utility:
-    def __init__(self, bot):
+class Utility(commands.Cog):
+    def __init__(self,bot):
         self.bot = bot
 
     async def prev_attachedimg(self, ctx):
@@ -23,29 +22,26 @@ class Utility:
             attachment = ctx.message.attachments[0]
         else:
             attach_msg = await ctx.history().find(lambda m: m.attachments)
-            if attach_msg is None:
-                raise commands.CommandError(f"Attachment not found.")
+            if attach_msg == None: raise commands.CommandError(f"Attachment not found.")
             attachment = attach_msg.attachments[0]
         return attachment
 
     @commands.command()
     @commands.has_permissions(manage_roles=True)
-    async def mute(self, ctx, members: commands.Greedy[discord.Member],
-                   minutes: typing.Optional[int] = 10, *, reason='no reason'):
+    async def mute(self, ctx, members: commands.Greedy[discord.Member], minutes: typing.Optional[int] = 10, *, reason = 'no reason'):
         '''Apply muted role'''
-        channel = self.bot.get_channel(555216652589858816)
         if not members:
             raise commands.CommandError("Needs user(s).")
         for member in members:
-            await member.add_roles(discord.Object(muted), reason=f"{ctx.author.name} gave the reason: {reason}")
+            await member.add_roles(discord.Object(muted),reason = f"{ctx.author.name} gave the reason: {reason}")
         if len(members) == 1:
             await ctx.send(f'{members[0].mention} was muted for {minutes} minute(s) because {reason}.')
+            channel = self.bot.get_channel(555216652589858816)
             await channel.send(f'{members[0].mention} was muted for {minutes} minute(s) because {reason}.')
         else:
-            await ctx.send((f'{", ".join(member.mention for member in members)} were '
-                            'muted for {minutes} minute(s) because {reason}.'))
-            await channel.send((f'{", ".join(member.mention for member in members)} were muted for {minutes}'
-                                ' minute(s) because {reason}.'))
+            await ctx.send(f'{", ".join(member.mention for member in members)} were muted for {minutes} minute(s) because {reason}.')
+            channel = self.bot.get_channel(555216652589858816)
+            await channel.send(f'{", ".join(member.mention for member in members)} were muted for {minutes} minute(s) because {reason}.')
         await asyncio.sleep(minutes*60)
         for member in members:
             await member.remove_roles(discord.Object(muted))
@@ -65,44 +61,43 @@ class Utility:
 
     @commands.command()
     @commands.has_permissions(kick_members=True)
-    async def kick(self, ctx, members: commands.Greedy[discord.Member], *, reason='no reason'):
+    async def kick(self, ctx, members: commands.Greedy[discord.Member], *, reason = 'no reason'):
         '''Kick member(s)'''
-        channel = self.bot.get_channel(555216652589858816)
         if not members:
             raise commands.CommandError("Needs user(s).")
         for member in members:
-            await member.kick(reason=f"{ctx.author.name} gave the reason: {reason}")
+            await member.kick(reason = f"{ctx.author.name} gave the reason: {reason}")
         if len(members) == 1:
             await ctx.send(f'{member.display_name} has been kicked because {reason}.')
+            channel = self.bot.get_channel(555216652589858816)
             await channel.send(f'{member.display_name} has been kicked because {reason}.')
         else:
-            kick_msg = f'{", ".join({member.display_name} for member in members)} have been kicked because {reason}.'
-            await ctx.send(kick_msg)
-            await channel.send(kick_msg)
+            await ctx.send(f'{", ".join({member.display_name} for member in members)} have been kicked because {reason}.')
+            channel = self.bot.get_channel(555216652589858816)
+            await channel.send(f'{", ".join({member.display_name} for member in members)} have been kicked because {reason}.')
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
-    async def ban(self, ctx, members: commands.Greedy[discord.Member], *, reason='no reason'):
+    async def ban(self, ctx, members: commands.Greedy[discord.Member], *, reason = 'no reason'):
         '''Ban member(s)'''
-        channel = self.bot.get_channel(555216652589858816)
         if not members:
             raise commands.CommandError("Needs user(s).")
         for member in members:
-            await member.ban(reason=f"{ctx.author.name} gave the reason: {reason}")
+            await member.ban(reason = f"{ctx.author.name} gave the reason: {reason}")
         if len(members) == 1:
             await ctx.send(f'{member.display_name} has been banned because {reason}.')
             channel = self.bot.get_channel(555216652589858816)
             await channel.send(f'{member.display_name} has been banned because {reason}.')
         else:
-            ban_msg = f'{", ".join({member.display_name} for member in members)} have been banned because {reason}.'
-            await ctx.send(ban_msg)
-            await channel.send(ban_msg)
+            await ctx.send(f'{", ".join({member.display_name} for member in members)} have been banned because {reason}.')
+            channel = self.bot.get_channel(555216652589858816)
+            await channel.send(f'{", ".join({member.display_name} for member in members)} have been banned because {reason}.')
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def prune(self, ctx, amount: int):
         '''Prune messages'''
-        await ctx.channel.purge(limit=amount + 1)
+        await ctx.channel.purge(limit = amount +1)
         message = await ctx.send(f"{amount} messages pruned.")
         await asyncio.sleep(4)
         await message.delete()
@@ -112,13 +107,13 @@ class Utility:
     async def steal(self, ctx, emote: typing.Optional[discord.PartialEmoji], name):
         '''Steal an emote from an existing emote or image'''
         if (emote):
-            async with ClientSession() as session, session.get(emote.url) as res:
-                image = io.BytesIO(await res.read())
+                async with ClientSession() as session, session.get(emote.url) as res:
+                    image = io.BytesIO(await res.read())
         else:
             attachment = await self.prev_attachedimg(ctx)
             async with ClientSession() as session, session.get(attachment.url) as res:
                 image = io.BytesIO(await res.read())
-        emote = await ctx.guild.create_custom_emoji(name=name, image=image.getvalue())
+        emote = await ctx.guild.create_custom_emoji(name = name, image = image.getvalue())
         await ctx.send(f"Emoji successfully created! {emote}")
 
     @commands.command()
@@ -128,9 +123,8 @@ class Utility:
         attachment = await self.prev_attachedimg(ctx)
         async with ClientSession() as session, session.get(attachment.url) as res:
             image = io.BytesIO(await res.read())
-            await self.self.bot.user.edit(avatar=image.getvalue())
+            await self.bot.user.edit(avatar=image.getvalue())
         await ctx.send(f'My avatar is changed!')
-
 
 def setup(bot):
     bot.add_cog(Utility(bot))
--- marlow_bot.py
+++ marlow_bot.py
@@ -11,75 +11,77 @@ client = discord.Client()
 
 TOKEN = config.TOKEN
 
-# stage changes & commit
-# git fetch origin master
-# git push origin master
-header = {"User-Agent": "Magic Browser"}
+header = {"User-Agent" : "Magic Browser"}
 heart = '<a:loading_heart:542883297600995349>'
-staff = {'Admin': 553314578528993311,
-         'Mod': 553356112636936203}
+staff = {'Admin':553314578528993311,
+        'Mod':553356112636936203}
 muted = 553358001550131208
+testing = 553314472341536776
 
 # This is how you define a discord.py event
 @bot.event
 async def on_ready():  # the event `on_ready` is triggered when the bot is ready to function
-    print("The bot is READY!")
-    print("Logged in as: {}".format(bot.user.name))
+    print(f"{format(bot.user.name)} is online.")
     bot.load_extension("cogs.Utility")
     bot.load_extension("cogs.Fun")
-    await bot.change_presence(activity=discord.Activity(name="Despacito", type=discord.ActivityType.listening))
-
+    await bot.change_presence(activity=discord.Activity(name="Despacito",type=discord.ActivityType.listening))
+    channel = bot.get_channel(testing)
+    await channel.send("Bot is online.")
 
 @bot.event
-async def on_command_error(ctx, e):
-    if isinstance(e, commands.CommandOnCooldown):
-        message = await ctx.send(f'Stop spamming! Try again in {round(e.retry_after)+1} second(s).')
-    elif isinstance(e, commands.CheckFailure):
-        message = await ctx.send(f"Fuck off! You're not my master!")
-    elif isinstance(e, commands.CommandNotFound):
-        message = await ctx.send(f"Command does not exist.")
-    elif isinstance(e, commands.MissingPermissions):
-        message = await ctx.send(f"You are missing these ({list.missing_perms}) to run this command.")
-    elif isinstance(e, commands.BotMissingPermissions):
-        message = await ctx.send(f"I need these permissions ({list.missing_perms}) to run this command.")
-    elif isinstance(e, commands.CommandError):
-        message = await ctx.send(f"**Error:** {e}")
-    elif isinstance(e, discord.HTTPException):
-        message = await ctx.send(f"**Error:** {e}")
-    else:
-        print(e)
-        return
-    await asyncio.sleep(5)
-    await message.delete()
-    try:
-        await ctx.message.delete()
-    except discord.HTTPException:
-        pass
+async def on_command_error(ctx,e):
+    if hasattr(ctx.command, 'on_error'):
+      print(type(e), e)
+      return
 
+    e = getattr(e, 'original', e)
+
+    ignored = (commands.CommandNotFound, commands.UserInputError)
+
+    if isinstance(e, ignored):
+      return
+
+    elif isinstance(e, commands.CommandOnCooldown):
+        return await ctx.send(f'Stop spamming! Try again in {round(e.retry_after)+1} second(s).')
+    elif isinstance(e, commands.CheckFailure):
+        return await ctx.send(f"I only respond to my mom.")
+    elif isinstance(e, commands.MissingPermissions):
+        return await ctx.send(f"You are missing these ({list.missing_perms}) to run this command.")
+    elif isinstance(e, commands.BotMissingPermissions):
+        return await ctx.send(f"I need these permissions ({list.missing_perms}) to run this command.")
+    elif isinstance(e, commands.DisabledCommand):
+        return await ctx.send(f"Command is disabled.")
+    elif isinstance(e, commands.CommandError):
+        return await ctx.send(f"**Error:** {e}")
+    else:
+        print(type(e), e)
+        return
+    #await asyncio.sleep(5)
+    #await message.delete()
+    #try: await ctx.message.delete()
+    #except discord.HTTPException: pass
 
 @bot.event
 async def on_message(message):
     if message.author.bot:
         return
-    if random.randrange(6) == 0:
+    if random.randrange(4) == 0:
         if 'uwu' in message.content:
             await message.channel.send('uwu')
         if 'OwO' in message.content:
             await message.channel.send('whats this?')
     await bot.process_commands(message)
 
-
 @bot.event
 async def on_member_join(member):
     channel = bot.get_channel(555216652589858816)
-    message = 'Hello {}! Please enjoy your stay.'.format(member.mention)
+    message ='Hello {}! Please enjoy your stay.'.format(member.mention)
     await channel.send(message)
-
 
 @bot.event
 async def on_member_remove(member):
     channel = bot.get_channel(555216652589858816)
-    message = 'Goodbye {}! We will miss you!'.format(member.mention)
+    message ='Goodbye {}! We will miss you!'.format(member.mention)
     await channel.send(message)
 
 initial_extensions = ["cogs.Utility,cogs.Fun"]
@@ -90,8 +92,7 @@ if __name__ == '__cogs__':
         try:
             bot.load_extension(extension)
         except Exception as error:
-            print('{} cannot be loaded. [{}]'.format(extension, error))
-
+            print('{} cannot be loaded. [{}]'.format(extension,error))
 
 @bot.command()
 @commands.is_owner()
@@ -101,9 +102,8 @@ async def unload(ctx, extension_name: str):
         bot.unload_extension(extension_dir)
         await ctx.send("{} unloaded.".format(extension_name))
     except Exception as error:
-        await ctx.send('{} cannot be unloaded. [{}]'.format(extension_name, error))
-        print('{} cannot be unloaded. [{}]'.format(extension_name, error))
-
+        await ctx.send('{} cannot be unloaded. [{}]'.format(extension_name,error))
+        print('{} cannot be unloaded. [{}]'.format(extension_name,error))
 
 @bot.command()
 @commands.is_owner()
@@ -113,21 +113,20 @@ async def load(ctx, extension_name: str):
         bot.load_extension(extension_dir)
         await ctx.send("{} loaded.".format(extension_name))
     except Exception as error:
-        await ctx.send('{} cannot be loaded. [{}]'.format(extension_name, error))
-        print('{} cannot be loaded. [{}]'.format(extension_name, error))
-
+        await ctx.send('{} cannot be loaded. [{}]'.format(extension_name,error))
+        print('{} cannot be loaded. [{}]'.format(extension_name,error))
 
 @bot.command()
 @commands.is_owner()
 async def update(ctx, extension_name: str):
     try:
         extension_dir = f"{cogs_dir}.{extension_name}"
-        bot.unload_extension(extension_dir)
-        bot.load_extension(extension_dir)
-        await ctx.send("{} unloaded and loaded".format(extension_name))
+        bot.reload_extension(extension_dir)
+        await ctx.send("{} updated.".format(extension_name))
     except Exception as error:
-        await ctx.send('{} cannot be loaded. [{}]'.format(extension_name, error))
-        print('{} cannot be loaded. [{}]'.format(extension_name, error))
+        await ctx.send('{} cannot be updated. [{}]'.format(extension_name, error))
+        print('{} cannot be updated. [{}]'.format(extension_name, error))
+
 
 # starts the bot with the corresponding token
 bot.run(TOKEN)
